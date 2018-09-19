be8c49abbc01379de7d27c80f349a94f87749ca0
Farhan <farhanisto@gmail.com>
2018-09-19 03:50:54 +0300

    updated mangoose connection errors

--- server.js
+++ server.js
@@ -11,8 +11,16 @@ const port = process.env.PORT || 8080;
 
 //DB connection setting with mongoose
 const options = {
-    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+    useNewUrlParser: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
   };
 
 mongoose.Promise =global.Promise;
